df6ac0eb8742aa6cd0b63390fd5d0e4472b45687
Namilskyy <alive6863@gmail.com>
2025-09-13 22:35:44 +0300

    Added CMake build

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-gcc -v -Wall -Wextra -Werror -lm -lmpfr -lgmp -I/usr/include -L/usr/lib src/main.c -o tpi-v2 -m32
+#gcc -v -Wall -Wextra -Werror -lm -lmpfr -lgmp -I/usr/include -L/usr/lib src/main.c -o tpi
+
+cmake -S . -B build/ 
+cd build/
+make 
